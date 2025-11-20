13a0dd940d06bdeb1fba2cbf05844e8120fea31e
Miika Nordblad <bhm247@myy.haaga-helia.fi>
2025-11-20 20:47:15 +0200

    Suunnittele sovelluksen komponenttirakenne ja lataa keskeiset koodikirjastot

---
+++ App.tsx
@@ -0,0 +1,26 @@
+import { StyleSheet } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { Provider as PaperProvider } from 'react-native-paper';
+import { AuthProvider } from './src/store/AuthContext';
+import RootNavigator from './src/navigation/RootNavigator';
+
+export default function App() {
+  return (
+    <PaperProvider>
+      <AuthProvider>
+        <NavigationContainer>
+          <RootNavigator />
+        </NavigationContainer>
+      </AuthProvider>
+    </PaperProvider>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
